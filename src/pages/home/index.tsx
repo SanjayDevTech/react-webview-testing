f13218179557813ff97dfd1328bf3a7d62d08e8d
SanjayDevTech <sureshsanjay805@gmail.com>
2021-08-03 17:56:07 +0530

    console.log(android)

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import useCookie from 'react-use-cookie';
 import computeName from '../../names';
@@ -6,6 +6,12 @@ import computeName from '../../names';
 export default function HomePage() {
     const [name, setName] = useCookie('name');
     const history = useHistory();
+
+    useEffect(() => {
+        const a = window.Android;
+        a?.showToast(JSON.stringify(a));
+    }, []);
+
     return (
         <div>
             <h1>Home Page</h1>
@@ -14,10 +20,8 @@ export default function HomePage() {
             <button
                 onClick={() => {
                     const name = computeName();
-                    const a = (window as any).Android;
-                    if (a) {
-                        a?.showToast(name);
-                    }
+                    const a = window.Android;
+                    a?.showToast(name);
                     setName(name);
                 }}>
                 Change Name
